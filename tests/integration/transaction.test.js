334649ebf65b0c28af4b21d50a65ea1af72e2ac3
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-11-28 11:31:45 -0300

    Test post transaction route when invalid token and invalid data

--- tests/integration/transaction.test.js
+++ tests/integration/transaction.test.js
@@ -19,6 +19,30 @@ afterAll(async () => {
 });
 
 describe('POST /transaction', () => {
+  test('should return 400 when the provided transaction data is invalid', async () => {
+    const token = await createSession();
+    const transaction = {};
+
+    const result = await supertest(app)
+      .post('/transaction')
+      .send(transaction)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(result.status).toEqual(400);
+  });
+
+  test('should return 401 when token is invalid', async () => {
+    const token = '';
+    const transaction = createTransaction();
+
+    const result = await supertest(app)
+      .post('/transaction')
+      .send(transaction)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(result.status).toEqual(401);
+  });
+
   test('should return 201 when the provided transaction data is valid', async () => {
     const token = await createSession();
     const transaction = createTransaction();
